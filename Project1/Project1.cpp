2cd9df4c7026002409c5e851954af3f782b2e61b
SDR_Fang <fanx@usc.edu>
2017-02-02 19:32:34 -0800

    Added Heuristic

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -1,4 +1,16 @@
 #include "Project1.h"
+#include "Robot.h"
+
+/**
+* @posX current X position
+* @posY current Y position
+* @tarX target X position
+* @tarY target Y position
+*/
+int Heuristic (int posX, int posY, int tarX, int tarY){
+	// Simply use distance as Heuristic
+	return (posX - tarX) * (posX - tarX) + (posY - tarY) * (posY - tarY);
+}
 
 /**
 * @brief default constructor
@@ -14,5 +26,9 @@ Project1::Project1(Simulator* sim1) {
  */
 RobotAction Project1::getOptimalAction(Simulator* sim1, Robot* r1) {
     // Here, you should find the next step of the robot.
-    return (RobotAction)(rand()%8);
+	int next_heuristic = 0;
+	RobotAction action = STOP;
+	// check actions
+	
+    return action;
 }
