aa932b80f44724f344ce9a99c13a3cff70291f5f
Fan Xu <fanx@usc.edu>
2017-02-02 19:24:39 -0800

    finished

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Project1.h"
 #include "Robot.h"
 
@@ -9,6 +10,7 @@
 */
 int Heuristic (int posX, int posY, int tarX, int tarY){
 	// Simply use distance as Heuristic
+	// std::cout << "Heuristic: (" << posX << ',' << posY << ") to (" << tarX << ',' << tarY << ")\n";
 	return (posX - tarX) * (posX - tarX) + (posY - tarY) * (posY - tarY);
 }
 
@@ -27,8 +29,70 @@ Project1::Project1(Simulator* sim1) {
 RobotAction Project1::getOptimalAction(Simulator* sim1, Robot* r1) {
     // Here, you should find the next step of the robot.
 	int next_heuristic = 0;
+	int temp_heuristic = 0;
 	RobotAction action = STOP;
+	Point2D target = sim1->getTarget();
 	// check actions
+	// MOVE_UP
+	next_heuristic = Heuristic(r1->X - 1, r1->Y, target.x, target.y);
+	action = MOVE_UP;
+	// std::cout << "MOVE_UP:" << next_heuristic << std::endl;
+	// MOVE_DOWN
+	temp_heuristic = Heuristic(r1->X + 1, r1->Y, target.x, target.y);
+	// std::cout << "MOVE_DOWN:" << temp_heuristic << std::endl;
+	if (temp_heuristic < next_heuristic){
+		next_heuristic = temp_heuristic;
+		action = MOVE_DOWN;
+	}
+	// MOVE_LEFT
+	temp_heuristic = Heuristic(r1->X, r1->Y - 1, target.x, target.y);
+	// std::cout << "MOVE_LEFT:" << temp_heuristic << std::endl;
+	if (temp_heuristic < next_heuristic){
+		next_heuristic = temp_heuristic;
+		action = MOVE_LEFT;
+	}
+	// MOVE_RIGHT
+	temp_heuristic = Heuristic(r1->X, r1->Y + 1, target.x, target.y);
+	// std::cout << "MOVE_RIGHT:" << temp_heuristic << std::endl;
+	if (temp_heuristic < next_heuristic){
+		next_heuristic = temp_heuristic;
+		action = MOVE_RIGHT;
+	}
+	// MOVE_UP_RIGHT
+	temp_heuristic = Heuristic(r1->X - 1, r1->Y + 1, target.x, target.y);
+	// std::cout << "MOVE_UP_RIGHT:" << temp_heuristic << std::endl;
+	if (temp_heuristic < next_heuristic){
+		next_heuristic = temp_heuristic;
+		action = MOVE_UP_RIGHT;
+	}
+	// MOVE_UP_LEFT
+	temp_heuristic = Heuristic(r1->X - 1, r1->Y - 1, target.x, target.y);
+	// std::cout << "MOVE_UP_LEFT:" << temp_heuristic << std::endl;
+	if (temp_heuristic < next_heuristic){
+		next_heuristic = temp_heuristic;
+		action = MOVE_UP_LEFT;
+	}
+	// MOVE_DOWN_RIGHT
+	temp_heuristic = Heuristic(r1->X + 1, r1->Y + 1, target.x, target.y);
+	// std::cout << "MOVE_DOWN_RIGHT:" << temp_heuristic << std::endl;
+	if (temp_heuristic < next_heuristic){
+		next_heuristic = temp_heuristic;
+		action = MOVE_DOWN_RIGHT;
+	}
+ 	// MOVE_DOWN_LEFT
+	temp_heuristic = Heuristic(r1->X + 1, r1->Y - 1, target.x, target.y);
+	// std::cout << "MOVE_DOWN_LEFT:" << temp_heuristic << std::endl;
+	if (temp_heuristic < next_heuristic){
+		next_heuristic = temp_heuristic;
+		action = MOVE_DOWN_LEFT;
+	}
+ 	// STOP
+	temp_heuristic = Heuristic(r1->X, r1->Y, target.x, target.y);
+	// std::cout << "STOP:" << temp_heuristic << std::endl;
+	if (temp_heuristic < next_heuristic){
+		next_heuristic = temp_heuristic;
+		action = STOP;
+	}
 	
     return action;
 }
